6630c941f25afaa0f7914ed762de86b217625030
wblaiklock <wblaiklock@gmail.com>
2025-01-06 13:38:12 -0500

    First

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const expresslayout = require('express-ejs-layouts')
+
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname +'/public/css'))
+app.use(expresslayout)
+app.use(bodyParser.urlencoded({ extended:false }))
+
+
+app.set('views','./src/views/')
+app.set('view engine','ejs')
+
+const newsRouter = require('./src/routes/news')
+
+app.use('/',newsRouter)
+app.use('/article',newsRouter)
+
+const port = process.env.PORT || 3030;
+app.listen(port,()=>console.log(`Listening port ${port}...`));
---
+++ src/routes/news.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router()
+const axios = require('axios')
+require('dotenv').config()
+const Joi = require('joi');
+
+const lang="en";
+
+router.get('/',async(req,res)=>{
+    try{
+         const newsAPI = await axios.get(`https://gnews.io/api/v4/top-headlines?&lang=${lang}&token=${process.env.APIKEY}`) 
+         await res.render('usernews',{ news : newsAPI.data,facts })
+    }catch(err){
+        console.log('Error',err.message)
+    }
+});
+
+// breaking news
+router.get('/news/breaking-news',async(req,res)=>{
+  try{
+       const newsAPI = await axios.get(`https://gnews.io/api/v4/top-headlines?&token=${process.env.APIKEY}&topic=breaking-news&lang=${lang}`) 
+       await res.render('usernews',{ news : newsAPI.data,facts })
+  }catch(err){
+    console.log('Error',err.message)
+  }   
+});
+
+
+//search
+router.post("/news/search",async(req,res)=>{
+  try{
+    const newsAPI = await axios.get(`https://gnews.io/api/v4/search?q="${req.body.search}"&token=${process.env.APIKEY}&lang=${lang}`)  
+    await res.render('usernews',{ news : newsAPI.data,facts })
+}catch(err){
+  console.log('Error',err.message)
+}
+});
+
+
+module.exports = router;
+
+
+
+ 
